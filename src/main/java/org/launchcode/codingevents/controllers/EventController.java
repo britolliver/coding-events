be633be6c23e187946fde70d574fa995e011a824
Bri <bri39annat@gmail.com>
2021-06-20 10:42:54 -0500

    commit after Bootstrap

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -19,13 +19,15 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
+       model.addAttribute("title", "All Events");
        model.addAttribute("events", events);
         return "events/index";
     }
 
     //lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String renderCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
     
